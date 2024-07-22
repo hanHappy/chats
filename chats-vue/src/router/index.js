abce44bd6976b0ef7700079b7b53ea53a63d6567
hanHappy <hansmin95@gmail.com>
2024-07-23 00:00:58 +0900

    Index 페이지 작성

--- chats-vue/src/router/index.js
+++ chats-vue/src/router/index.js
@@ -6,10 +6,12 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/Home.vue"),
+    },
+    {
+      path: "/chats",
+      name: "chats",
+      component: () => import("../views/Chat.vue"),
     },
   ],
 });
--- chats-vue/src/views/Home.vue
+++ chats-vue/src/views/Home.vue
@@ -1,5 +1,19 @@
 <script setup></script>
 <template>
-  <h1>채팅방 입장</h1>
+  <main>
+    <div class="container h-screen mx-auto py-40">
+      <h1 class="mx-auto text-5xl text-[#979797] font-black text-center">
+        채팅 APP
+      </h1>
+      <div class="mt-20 flex items-center justify-center">
+        <RouterLink
+          to="/chats"
+          class="bg-[#40D393] text-[#223547] w-fit py-2 px-4 rounded-lg font-bold mx-auto hover:bg-[#32be81]"
+        >
+          채팅방 입장
+        </RouterLink>
+      </div>
+    </div>
+  </main>
 </template>
-<style scoped></style>
+<style></style>
