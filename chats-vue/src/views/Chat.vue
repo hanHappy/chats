93f07b1a400b8af110ac27ffb750752d20feca18
hanHappy <hansmin95@gmail.com>
2024-07-23 12:13:41 +0900

    Chat.vue 작성 중

---
+++ chats-vue/src/views/Chat.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+<template>
+  <div class="container h-screen mx-auto w-1/3">
+    <header class="h-16 sticky top-0 flex items-center justify-between">
+      <div class="text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9.75 14.25 12m0 0 2.25 2.25M14.25 12l2.25-2.25M14.25 12 12 14.25m-2.58 4.92-6.374-6.375a1.125 1.125 0 0 1 0-1.59L9.42 4.83c.21-.211.497-.33.795-.33H19.5a2.25 2.25 0 0 1 2.25 2.25v10.5a2.25 2.25 0 0 1-2.25 2.25h-9.284c-.298 0-.585-.119-.795-.33Z"
+          />
+        </svg>
+      </div>
+      <h1 class="text-white text-lg">유진하</h1>
+      <div class="text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
+    </header>
+    <div class="chat-room-bg mx-auto"></div>
+  </div>
+</template>
+<style scoped>
+</style>
