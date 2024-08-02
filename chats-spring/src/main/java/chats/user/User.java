ece9602772db949328f179513b8e0d5869d24b4a
hsm <hsm@genexon.co.kr>
2024-08-02 19:34:21 +0900

    User package 작성 중

---
+++ chats-spring/src/main/java/chats/user/User.java
@@ -0,0 +1,25 @@
+package chats.user;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String username;
+    private String password;
+}
---
+++ chats-spring/src/main/java/chats/user/UserService.java
@@ -0,0 +1,8 @@
+package chats.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
