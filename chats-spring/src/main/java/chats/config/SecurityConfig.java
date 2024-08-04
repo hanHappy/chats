aa6b23929503da33e17abbf26f6756d453689286
hanHappy <hansmin95@gmail.com>
2024-08-04 23:34:52 +0900

    SecurityConfig 작성

--- chats-spring/src/main/java/chats/config/SecurityConfig.java
+++ chats-spring/src/main/java/chats/config/SecurityConfig.java
@@ -1,17 +1,30 @@
 package chats.config;
 
+import chats.filter.JwtAuthenticationFilter;
+import chats.security.handler.JwtAccessDeniedHandler;
+import chats.security.handler.JwtAuthenticationEntryPoint;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
- * {@code EnableWebSecurity} <br/>
- * {@code Configuration} 클래스에 추가하여 {@code WebSecurityConfigurer}에 정의된 Spring Security 설정을 사용한다.
- * 즉 {@code SecurityFilterChain} bean으로 등록한다. <br/>
- * <br/>
- * {@code @EnableMethodSecurity} <br/>
- * 메서드 수준의 보안을 활성화한다. {@code @PreAuthorize}, {@code @PostAuthorize}, {@code @PreFilter}, {@code
- * PostFilter} 를 제공하여 메서드 호출 전, 후에 보안 검사를 수행할 수 있다.
+ * {@code EnableWebSecurity} <br/> {@code Configuration} 클래스에 추가하여 {@code WebSecurityConfigurer}에
+ * 정의된 Spring Security 설정을 사용한다. 즉 {@code SecurityFilterChain} bean으로 등록한다. <br/> <br/>
+ * {@code @EnableMethodSecurity} <br/> 메서드 수준의 보안을 활성화한다. {@code @PreAuthorize},
+ * {@code @PostAuthorize}, {@code @PreFilter}, {@code PostFilter} 를 제공하여 메서드 호출 전, 후에 보안 검사를 수행할 수
+ * 있다.
  *
  * @see <a
  * href="https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/config/annotation/web/configuration/EnableWebSecurity.html">EnableWebSecurity</a>
@@ -22,7 +35,47 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll()
+                // .requestMatchers("/api/user/**").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling(exception -> exception
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    /**
+     * FIXME
+     *  배포 시 요청 url 수정
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- chats-spring/src/main/java/chats/constants/ErrorCode.java
+++ chats-spring/src/main/java/chats/constants/ErrorCode.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+    INTERNAL_SERVER_ERROR("ERR_001", HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생하였습니다"),
+
     // 인증
     UNAUTHORIZED("AUTH_001", HttpStatus.UNAUTHORIZED, "인증에 실패하였습니다"),
     FORBIDDEN("AUTH_002", HttpStatus.FORBIDDEN, "권한이 없습니다"),
