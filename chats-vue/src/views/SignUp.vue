d4776006326ba349acec9417890a9b5ec41abcb6
hanHappy <hansmin95@gmail.com>
2024-08-06 08:44:40 +0900

    회원 가입 페이지 작성 중

---
+++ chats-vue/src/views/SignUp.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="mt-4">
+    <label>
+      <span class="color-2">ID</span>
+      <input type="text" class="w-full mt-2" v-model.trim="userId" @keyup.enter="enter" autofocus>
+    </label>
+  </div>
+  <div class="mt-4">
+    <label>
+      <span class="color-2">PW</span>
+      <input type="password" class="w-full mt-2" v-model.trim="password" @keyup.enter="enter"
+             autofocus>
+    </label>
+  </div>
+  <div class="mt-16">
+    <button class="w-full" @click="enter">Sign Up</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
